0725e42b49c254dac77c22d55b303c88396a0501
Matheus Barboza <matheus.barboza102@gmail.com>
2022-07-13 21:03:58 -0300

    controller e models

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express'
+import { CreateCategoryController } from './controllers/CreateCategoryController'
+import { CreateProductCategoryController } from './controllers/CreateProductCategoryController'
 import { CreateProductController } from './controllers/CreateProductController'
 
 const router = Router()
 
 const createProduct = new CreateProductController()
+const createCategory = new CreateCategoryController()
+const createProductCategoryController = new CreateProductCategoryController()
 
 router.post('/product', createProduct.handle)
+router.post('/category', createCategory.handle)
+router.post('/productCategory', createProductCategoryController.handle)
 
 export { router }
